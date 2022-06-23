9676a120d4448228537516598d5af407016b4736
Hyun-Sang Lee <coolwin200@gmail.com>
2022-06-23 17:41:21 +0900

    auth2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "amplifyappbb99cb9d": {
+        "amplifyapp": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
